b350a33b28d6acb3107d951bd9593bde4682c763
lll-jy <E0376997@u.nus.edu>
2020-10-17 12:25:21 +0800

    Create new class to represent person name

---
+++ src/main/java/seedu/address/model/person/PersonName.java
@@ -0,0 +1,59 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents a Project's name in the main catalogue.
+ * Guarantees: immutable; is valid as declared in {@link #isValidPersonName(String)}
+ */
+public class PersonName {
+
+    public static final String MESSAGE_CONSTRAINTS =
+            "Names should only contain alphanumeric characters and spaces, and it should not be blank";
+
+    /*
+     * The first character of the address must not be a whitespace,
+     * otherwise " " (a blank string) becomes a valid input.
+     */
+    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
+
+    public final String fullPersonName;
+
+    /**
+     * Constructs a {@code PersonName}.
+     *
+     * @param personName A valid personName.
+     */
+    public PersonName(String personName) {
+        requireNonNull(personName);
+        checkArgument(isValidPersonName(personName), MESSAGE_CONSTRAINTS);
+        fullPersonName = personName;
+    }
+
+    /**
+     * Returns true if a given string is a valid name.
+     */
+    public static boolean isValidPersonName(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+
+    @Override
+    public String toString() {
+        return fullPersonName;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof PersonName // instanceof handles nulls
+                && fullPersonName.equals(((PersonName) other).fullPersonName)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return fullPersonName.hashCode();
+    }
+
+}
